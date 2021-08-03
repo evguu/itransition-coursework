888c052b64b115ce9859f5cbbf86d071b2b6825e
Litnine <savchenko2002je@gmail.com>
2021-08-03 12:08:08 +0300

    login&logout work; CSRF token added as cookie

--- src/main/java/org/litnine/coursework/config/SecurityConfig.java
+++ src/main/java/org/litnine/coursework/config/SecurityConfig.java
@@ -1,14 +1,16 @@
 package org.litnine.coursework.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
         http
                 .authorizeRequests(a -> a
                         .antMatchers("/", "/error", "/webjars/**").permitAll()
@@ -16,6 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
                 .oauth2Login();
     }
