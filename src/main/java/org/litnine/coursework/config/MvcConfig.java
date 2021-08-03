922a63473039c20174c8390ee1cf24fcdf9d5969
Litnine <savchenko2002je@gmail.com>
2021-08-03 11:23:34 +0300

    securityconfig and container on auth

--- src/main/java/org/litnine/coursework/config/MvcConfig.java
+++ src/main/java/org/litnine/coursework/config/MvcConfig.java
@@ -11,7 +11,6 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
     }
 
 }
---
+++ src/main/java/org/litnine/coursework/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.litnine.coursework.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+    }
+}
