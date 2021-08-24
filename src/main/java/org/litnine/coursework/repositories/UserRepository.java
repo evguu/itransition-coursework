eaca97004d4ef9fd43fd6beeb7b648e4141c204d
Litnine <savchenko2002je@gmail.com>
2021-08-24 15:03:06 +0300

    more i18n

--- src/main/java/org/litnine/coursework/repositories/UserRepository.java
+++ src/main/java/org/litnine/coursework/repositories/UserRepository.java
@@ -3,10 +3,21 @@ package org.litnine.coursework.repositories;
 import org.litnine.coursework.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.Optional;
 
+@PreAuthorize("hasRole('ROLE_USER')")
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findById(String id);
+        @PreAuthorize("hasRole('ROLE_ADMIN')")
+        @Override
+        void deleteById(String id);
+
+        @PreAuthorize("hasRole('ROLE_ADMIN')")
+        @Override
+        void delete(User user);
+
+        @Override
+        Optional<User> findById(String id);
 }
